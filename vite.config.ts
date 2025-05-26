4e947654c7fa78e026880f6e2738df50f7113e53
QUTBITours <qtholidays@gmail.com>
2025-05-26 17:23:33 +0530

    Build and deploy app

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/qt-holidays-app/', // GitHub Pages repository name
+  base: '/B/', // <-- update this to your repo name!
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
